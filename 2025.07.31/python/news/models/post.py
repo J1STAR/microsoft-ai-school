53eff12857786909ab81cb22657c00c73c9f5fdf
HanByeol Jang <j.1star.0726@gmail.com>
2025-08-05 15:50:42 +0900

    feat: Post model에 removed_at 필드 추가

--- 2025.07.31/python/news/models/post.py
+++ 2025.07.31/python/news/models/post.py
@@ -5,3 +5,5 @@ class Post(BaseModel):
     title = models.CharField(max_length=100, verbose_name="제목")
     content = models.TextField(verbose_name="내용", blank=True, null=True)
     author = models.ForeignKey("User", on_delete=models.CASCADE, verbose_name="작성자")
+
+    removed_at = models.DateTimeField(verbose_name="삭제일시", blank=True, null=True)
